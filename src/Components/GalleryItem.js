0c0780f9f38f9b97f2fa70c7c130fd1474168659
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2023-01-11 20:54:36 -0800

    Added Suspense

--- src/Components/GalleryItem.js
+++ src/Components/GalleryItem.js
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import { useState } from 'react' 
 import { Link } from 'react-router-dom'
 
-function GalleryItem(props) {
+function GalleryItem(props){
     let [view, setView] = useState(false)
 
     const simpleStyle = {
@@ -21,7 +21,6 @@ function GalleryItem(props) {
         'backgroundSize': 'cover',
         'color': 'yellow'
     }
-    
 
     const simpleView = () => {
         return (
@@ -31,16 +30,16 @@ function GalleryItem(props) {
             </div>
         )
     }
-    
+
     const detailView = () => {
         return (
             <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
                 <h3>
-                    <Link to={`/artist/${props.item.artistId}`}>
+                        <Link to={`/artist/${props.item.artistId}`}>
                         {props.item.artistName}
-                    </Link>
-                </h3>
+                        </Link>
+                    </h3>
                 <h3>
                     <Link to={`/album/${props.item.collectionId}`}>
                         {props.item.collectionName}
@@ -51,18 +50,13 @@ function GalleryItem(props) {
             </div>
         )
     }
-    
-    
 
     return (
-        <div onClick={() => setView(!view)}
-        style={{'display': 'inline-block'}}>
-        
-            {/* This simple ternary shows the simple view when 'view' is false! */}
-            {view ? detailView() : simpleView()}
-
+        <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
+            <p>One Gallery Item</p>
+                {view ? detailView() : simpleView()}
         </div>
     )
-
 }
+
 export default GalleryItem
---
+++ src/Components/Spinner.js
@@ -0,0 +1,11 @@
+import loading_spinner from 'https://miro.medium.com/max/882/1*9EBHIOzhE1XfMYoKz1JcsQ.gif'
+
+const Spinner = () => {
+    return (
+        <div>
+            <img src={loading_spinner} alt="loading..." />
+        </div>
+    )
+}
+
+export default Spinner
